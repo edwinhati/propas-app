9916ce22d15ebc4a8491c71233d0d007b62e290f
edwinhati <edwinhati@outlook.com>
2023-11-27 13:52:55 +0800

    Add font-awesome stylesheet to layout component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,13 +6,13 @@ export default function Footer() {
       <div className="mx-auto max-w-screen-xl">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-            <Image
+            {/* <Image
               src="/logo.jpeg"
               className="mr-3 mb-3"
               alt="PROPAS Indonesia Logo"
               width={80}
               height={80}
-            />
+            /> */}
             <span className="self-center text-2xl font-semibold whitespace-nowrap">
               PROPAS INDONESIA
             </span>
@@ -20,7 +20,7 @@ export default function Footer() {
           <div className="">
             <div>
               <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
-                ALAMAT
+                Sekretariat PROPAS INDONESIA
               </h2>
               <address className="text-gray-600">
                 <p className="mb-4">
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,110 +4,121 @@ import Image from "next/image";
 import Link from "next/link";
 import { getProfile } from "@/config";
 
-interface User {
-  name: string;
-}
-
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [profile, setProfile] = useState<User>({
-    name: "",
-  });
+  const [profile, setProfile] = useState(null);
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   useEffect(() => {
-    const fetchProfile = async () => {
-      setProfile(await getProfile());
+    const getProfileData = async () => {
+      const profileData = await getProfile();
+      setProfile(profileData);
     };
-
-    fetchProfile();
+    getProfileData();
   }, []);
 
   return (
-    <nav className="bg-white border-gray-200 ">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <div className="flex items-center">
-          <Image
-            src="/logo.jpeg"
-            alt="Propas Logo"
-            width={40}
-            height={40}
-            priority
-          />
-          <div className="ml-4">
-            <h1 className="text-lg font-semibold whitespace-nowrap text-black">
-              PROPAS INDONESIA
-            </h1>
-            <p></p>
-          </div>
-        </div>
-        <button
-          onClick={toggleMenu} // Added onClick event to toggle the menu
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-          aria-controls="navbar-default"
-          aria-expanded={isMenuOpen} // Set the aria-expanded attribute based on menu state
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
-        <div
-          className={`w-full md:w-auto ${
-            isMenuOpen ? "block" : "hidden"
-          } md:block`}
-          id="navbar-default"
-        >
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white ">
-            <li>
-              <Link
-                href="/"
-                className="block py-2 pl-3 pr-4 text-white bg-red-700 rounded md:bg-transparent md:text-red-700 md:p-0"
-                aria-current="page"
-              >
-                Beranda
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/#news"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0"
-              >
-                Berita{" "}
-              </Link>
-            </li>
-
-            <li>
-              {profile.name !== "" ? (
-                <div className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0">
-                  Hi, {profile.name.split(" ")[0]}
-                </div>
-              ) : (
+    <header>
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <Link href="/" className="flex items-center">
+            <Image src="/logo.jpeg" alt="Propas Logo" width={40} height={40} />
+            <span className="ml-2 self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              {profile ? "PROPAS INDONESIA" : ""}
+            </span>
+          </Link>
+          <div className="flex items-center lg:order-2">
+            {!profile ? (
+              <>
                 <Link
                   href="/login"
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0"
+                  className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
                 >
-                  Log in{" "}
+                  Log in
                 </Link>
+                <Link
+                  href="/register"
+                  className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800"
+                >
+                  Daftar
+                </Link>
+              </>
+            ) : null}{" "}
+            <button
+              data-collapse-toggle="mobile-menu-2"
+              type="button"
+              onClick={toggleMenu}
+              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="mobile-menu-2"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {!isMenuOpen ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1.2em"
+                  viewBox="0 0 448 512"
+                >
+                  <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1.2em"
+                  viewBox="0 0 384 512"
+                >
+                  <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+                </svg>
               )}
-            </li>
-          </ul>
+            </button>
+          </div>
+          <div  
+            className={`${
+              !isMenuOpen && "hidden"
+            } justify-between items-center w-full lg:flex lg:w-auto lg:order-1`}
+            id="mobile-menu-2"
+          >
+            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+              <li>
+                <Link
+                  href="#"
+                  className="block py-2 pr-4 pl-3 text-white rounded bg-red-700 lg:bg-transparent lg:text-red-700 lg:p-0 dark:text-white"
+                  aria-current="page"
+                >
+                  Beranda
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-red-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Profil
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-red-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Berita & Artikel
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  href="#"
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-red-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Kontak
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 }
